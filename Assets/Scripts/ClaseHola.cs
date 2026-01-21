0a9f14b8d83136af19202005f07bc867dd30a57c
Paco <jpkogutierrez@gmail.com>
2026-01-21 15:03:00 -0600

    Input Action

--- Assets/Scripts/ClaseHola.cs
+++ Assets/Scripts/ClaseHola.cs
@@ -1,25 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class ClaseHola : MonoBehaviour
 {
-    [Header("Jugador")]
+    [SerializeField] private int _playerSpeed;
+    private Vector2 _input;
+    private PlayerInput _playerInput;
+
+    /*[Header("Jugador")]
     [SerializeField] private int velocidad;
 
     private Vector3 movim = new Vector3(1, 0, 0);
-    private Rigidbody2D rigbody;
+    private Rigidbody2D rigbody;*/
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Start");
-        rigbody = GetComponent<Rigidbody2D>();
+        _playerInput = GetComponent<PlayerInput>();
+        //rigbody = GetComponent<Rigidbody2D>();
         //transform.Translate(new Vector3(10, 1));
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        //transform.position += movim * Time.deltaTime;
-        rigbody.linearVelocity = movim*10;
+        ReadInput();
+
+        Debug.Log("Input: " + _input);
+
+        transform.position += new Vector3(_input.x,_input.y,0) * Time.deltaTime * _playerSpeed;
+        //rigbody.linearVelocity = movim * velocidad;
+    }
+
+    private void ReadInput() {
+        _input = _playerInput.actions["Move"].ReadValue<Vector2>();
+
     }
 }
